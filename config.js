befad8bc5558ab31b6d54603b067c0777912c6b7
turpah86 <turpah86@gmail.com>
2026-05-04 13:16:00 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const USERS = {
+    "ОГР-23": {
+        password: "123",
+        redirect: "ogr23.html",
+        quiz: "ogr23"
+    },
+    "РГО-24": {
+        password: "456",
+        redirect: "rgo24.html",
+        quiz: "rgo24"
+    },
+    "Admin": {
+        password: "6426",
+        redirect: "admin.html"
+    }
+};
