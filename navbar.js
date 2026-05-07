f16244ce23bb1bd3f4251497c187fddef8882852
turpah86 <turpah86@gmail.com>
2026-05-07 12:54:48 +0900

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const navHTML = `
+    // 1. HTML структура (Меню + Модалка)
+    const uiHTML = `
     <nav class="glass-nav">
         <div class="nav-container">
             <div class="logo">
                 <a href="index.html">
-                    <img src="logo-ggt.png" alt="Лого" style="height: 45px; width: auto;">
+                    <img src="logo-ggt.png" alt="Лого" style="height: 40px; width: auto;">
                 </a>
             </div>
             <div class="nav-links">
                 <a href="index.html">Главная</a>
-                <a href="students.html">Студентам</a>
+                <a href="students.html">Для студентов</a>
                 <a href="achievements.html">Достижения</a>
                 <a href="javascript:void(0)" onclick="openFeedback()" class="nav-contact">💬 Связь</a>
             </div>
