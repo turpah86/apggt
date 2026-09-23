b83cff7dbdd5992562b3329761b66fa60654612b
turpah86 <turpah86@gmail.com>
2026-09-23 09:06:18 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,31 @@
-const USERS = {
-    "огр": {
-        password: "123",
-        redirect: "ogr23.html",
-        quizPrefix: "ogr_razdel"
-    },
-    "рго": {
-        password: "456",
-        redirect: "rgo24.html",
-        quiz: "rgo24"
-    },
-    "admin": {
-        password: "6426",
-        redirect: "admin.html"
-    }
-};
 const S_URL = 'https://uqekwkidsuargeulwkuw.supabase.co';
 const S_KEY = 'sb_publishable_WefHBfPDA7Fx9ZDRMnZDUA_FfqINH2r';
 const _supabase = supabase.createClient(S_URL, S_KEY);
+// ГЛОБАЛЬНАЯ КАРТА ВСЕХ КУРСОВ ТЕХНИКУМА
+const GLOBAL_COURSES = {
+    "огр": {
+        title: "Направление: Открытые горные работы",
+        themes: [
+            { name: "Введение в технологию ОГР", link: "https://yandex.ru" },
+            { name: "Подготовка горных пород к выемке", link: "https://yandex.ru" },
+            { name: "Выемочно-погрузочные работы на карьере", link: "https://yandex.ru" }
+        ]
+    },
+    "рго": {
+        title: "Направление: Ремонт горного оборудования",
+        themes: [
+            { name: "Понятие об энергосистеме и электросетях", link: "https://yandex.ru" },
+            { name: "Схемы внешних распределительных сетей", link: "https://yandex.ru" },
+            { name: "Определение электрических нагрузок предприятия", link: "https://yandex.ru" }
+        ]
+    },
+    "rem": { // Твой изначальный курс slesar
+        title: "Электротехника и Технология ремонта",
+        themes: [
+            { name: "Основные понятия и закон Ома", link: "https://yandex.ru" },
+            { name: "Законы Кирхгофа", link: "https://yandex.ru" },
+            { name: "Введение в технологию ремонта горных машин", link: "https://yandex.ru" }
+        ]
+    }
+    // Сюда можно добавлять новые группы без переписывания страниц сайта!
+};
